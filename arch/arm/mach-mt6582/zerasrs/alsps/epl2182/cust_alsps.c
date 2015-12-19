c4011fbcbb0470bb9f14fc24527afdb0a24f0640
assusdan <assusdan@yandex.ru>
2015-12-19 13:52:48 +0500

    fix error

--- arch/arm/mach-mt6582/zerasrs/alsps/epl2182/cust_alsps.c
+++ arch/arm/mach-mt6582/zerasrs/alsps/epl2182/cust_alsps.c
@@ -1,6 +1,7 @@
 #include <linux/types.h>
 #include <mach/mt_pm_ldo.h>
 #include <cust_alsps.h>
+#include <mach/upmu_common.h>
 
 static struct alsps_hw cust_alsps_hw = {
     .i2c_num    = 2,
@@ -8,12 +9,18 @@ static struct alsps_hw cust_alsps_hw = {
 	.polling_mode_als =1,
     .power_id   = MT65XX_POWER_NONE,    /*LDO is not used*/
     .power_vol  = VOL_DEFAULT,          /*LDO is not used*/
-    .als_level  = { 0,  1,  1,   7,  15,  15,  100, 1000, 2000,  3000,  6000, 10000, 14000, 18000, 20000},
-    .als_value  = {40, 40, 90,  90, 160, 160,  225,  320,  640,  1280,  1280,  2600,  2600, 2600,  10240, 10240},
-    .ps_threshold_high = 4800,
-    .ps_threshold_low = 4500,
+    .als_level  = {5,  9, 36, 59, 82, 132, 205, 273, 500, 845, 1136, 1545, 2364, 4655, 6982},	/* als_code */
+    .als_value  = {0, 10, 40, 65, 90, 145, 225, 300, 550, 930, 1250, 1700, 2600, 5120, 7680, 10240},    /* lux */    
+    .ps_threshold_high = 288,
+    .ps_threshold_low = 240,
 };
-struct alsps_hw get_cust_alsps_hw(void) {
+struct alsps_hw *get_cust_alsps_hw(void) {
     return &cust_alsps_hw;
 }
-
+int pmic_ldo_suspend_enable(int enable)
+{
+	//0 for disable suspend, 1 for enable suspend
+	upmu_set_vio18_lp_sel(enable);
+	upmu_set_vio28_lp_sel(enable);
+	return 0;
+}
