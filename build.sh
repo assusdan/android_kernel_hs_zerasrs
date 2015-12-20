f55d905c73f030a5c94d6a0a7e5093d3cabb430b
Suslov Danila <assusdan@yandex.ru>
2015-12-20 11:10:55 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,7 @@ export ARCH=arm CROSS_COMPILE=../toolchain/linaro-4.9/bin/arm-linux-androideabi-
 make zerasrs_dijing_defconfig
 make -j4 zImage
 mv arch/arm/boot/zImage /var/www/html/zImage_DJN
+
+make zerasrs_cs_defconfig
+make -j4 zImage
+mv arch/arm/boot/zImage /var/www/html/zImage_CS
