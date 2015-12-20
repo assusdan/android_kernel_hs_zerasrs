74fe674fa960a7c0a8a62b699ebd21e66adccfd3
assusdan <assusdan@yandex.ru>
2015-12-20 10:30:22 +0500

    mod script

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 git pull
 export ARCH=arm CROSS_COMPILE=../toolchain/linaro-4.9/bin/arm-linux-androideabi-
-make zerasrs_cs_defconfig
-make -j4 zImage
-mv arch/arm/boot/zImage /var/www/html/zImage_CS
-make zerasrs_djn_defconfig
+
+make zerasrs_dijing_defconfig
 make -j4 zImage
 mv arch/arm/boot/zImage /var/www/html/zImage_DJN
