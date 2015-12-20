f30d76b889aaa3bae991afb73fca47d2b142fa1a
Suslov Danila <assusdan@yandex.ru>
2015-12-20 11:24:37 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
+echo 'Get changes from GitHub'
 git pull
+echo 'Start building...'
 export ARCH=arm CROSS_COMPILE=../toolchain/linaro-4.9/bin/arm-linux-androideabi-
 
-make zerasrs_dijing_defconfig
-make -j4 zImage
-mv arch/arm/boot/zImage /var/www/html/zImage_DJN
 
-make zerasrs_cs_defconfig
-make -j4 zImage
-mv arch/arm/boot/zImage /var/www/html/zImage_CS
+echo 'Configure DJN'
+make zerasrs_dijing_defconfig >/dev/null
+echo 'Build DJN'
+make -j4 zImage >/dev/null 2>buildlog.log
+
+if [ ! -f arch/arm/boot/zImage ]
+then
+    echo "BUID ERRORS!"
+fi
+
+echo 'Moving DJN'
+mv arch/arm/boot/zImage /var/www/html/zImage_DJN 
+
+echo 'Configure CS'
+make zerasrs_cs_defconfig >/dev/null
+echo 'Build CS'
+make -j4 zImage >/dev/null 2>buildlog.log
+
+if [ ! -f arch/arm/boot/zImage ]
+then
+    echo "BUID ERRORS!"
+fi
+
+echo 'Moving CS'
+mv arch/arm/boot/zImage /var/www/html/zImage_CS 
+
+#echo 'Configure A'
+#make zerasra_defconfig >/dev/null
+#echo 'Build A'
+#make -j4 zImage >/dev/null 2>buildlog.log
+#echo 'Moving A'
+#mv arch/arm/boot/zImage /var/www/html/zImage_A 
